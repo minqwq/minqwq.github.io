f4b8170f9aa399759aa4f0061efb7f013c0a96f2
minqwq <153706992+minqwq@users.noreply.github.com>
2024-03-20 04:05:37 +0800

    Delete javascript directory

--- javascript/rolling_title.js
+++
@@ -1,8 +0,0 @@
-        var text=document.title
-        var timerID
-        function newtext() {
-        clearTimeout(timerID)
-        document.title=text.substring(1,text.length)+text.substring(0,1)
-        text=document.title.substring(0,text.length)
-        timerID = setTimeout("newtext()", 200)
-        }
